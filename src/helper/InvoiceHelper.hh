85727d792adc4f8b7d24ba1791cccbd701e733fe
Jens Konopka <jens.konopka@plentymarkets.com>
2016-09-07 15:28:40 +0200

    new payment plugin

---
+++ src/helper/InvoiceHelper.hh
@@ -0,0 +1,48 @@
+<?hh //strict
+
+namespace Invoice\Helper;
+
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
+use Plenty\Modules\Payment\Method\Models\PaymentMethod;
+
+class InvoiceHelper
+{
+    private PaymentMethodRepositoryContract $paymentMethodRepository;
+
+    public function __construct(PaymentMethodRepositoryContract $paymentMethodRepository)
+    {
+        $this->paymentMethodRepository = $paymentMethodRepository;
+    }
+
+    public function createMopIfNotExists():void
+    {
+        if($this->getMop() == 'no_paymentmethod_found')
+        {
+            $paymentMethodData = array( 'pluginKey' => 'plenty_invoice',
+                'paymentKey' => 'INVOICE',
+                'name' => 'Rechnung');
+
+            $this->paymentMethodRepository->createPaymentMethod($paymentMethodData);
+        }
+    }
+
+    public function getMop():mixed
+    {
+        $paymentMethods = $this->paymentMethodRepository->allForPlugin('plenty_invoice');
+
+        if(count($paymentMethods))
+        {
+            foreach($paymentMethods as $paymentMethod)
+            {
+                if($paymentMethod->paymentKey == 'INVOICE')
+                {
+                    return $paymentMethod->id;
+                }
+            }
+        }
+
+        return 'no_paymentmethod_found';
+    }
+
+}
---
+++ src/providers/InvoiceServiceProvider.hh
@@ -0,0 +1,35 @@
+<?hh // strict
+
+namespace Invoice\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
+use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodContainer;
+use Invoice\Methods\InvoicePaymentMethod;
+use Invoice\Helper\InvoiceHelper;
+
+
+/**
+ * Class InvoiceServiceProvider
+ * @package Invoice\Providers
+ */
+ class InvoiceServiceProvider extends ServiceProvider
+ {
+     public function register():void
+     {
+
+     }
+
+     public function boot(InvoiceHelper $paymentHelper,
+                          PaymentMethodContainer $payContainer):void
+     {
+       $paymentHelper->createMopIfNotExists();
+
+       $payContainer->register('plenty_invoice::INVOICE', InvoicePaymentMethod::class,
+           [ \Plenty\Modules\Basket\Events\Basket\AfterBasketChanged::class,
+             \Plenty\Modules\Basket\Events\Basket\AfterBasketCreate::class]);
+     }
+ }
