a350dd16a7b1ffcd58a5547526245c94361e4828
Dietmar Langhuth <dietmar.langhuth@plentymarkets.com>
2017-07-26 15:12:40 +0200

    FIX for language dependency on infoPageType

--- src/Methods/InvoicePaymentMethod.php
+++ src/Methods/InvoicePaymentMethod.php
@@ -166,7 +166,7 @@ class InvoicePaymentMethod extends PaymentMethodService
         $session = pluginApp(FrontendSessionStorageFactoryContract::class);
         $lang = $session->getLocaleSettings()->language;
 
-        $infoPageType = $this->settings->getSetting('infoPageType');
+        $infoPageType = $this->settings->getSetting('infoPageType', $lang);
 
         switch ($infoPageType)
         {
