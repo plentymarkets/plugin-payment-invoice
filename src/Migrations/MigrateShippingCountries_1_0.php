1522e05f4613db819173daa05751d24d8cb74783
Marcel Pönninghaus <>
2019-02-28 19:27:46 +0100

    cleanup

--- src/Migrations/MigrateShippingCountries_1_0.php
+++ src/Migrations/MigrateShippingCountries_1_0.php
@@ -28,7 +28,6 @@ class MigrateShippingCountries_1_0
             $this->getLogger(__CLASS__.'::'.__FUNCTION__)->debug('Invoice::PaymentMethod.paymentMethodName', $ex->getMessage().' - '.$ex->getTraceAsString());
             $this->getLogger(__CLASS__.'::'.__FUNCTION__)->error('Invoice::PaymentMethod.paymentMethodName', $ex->getMessage().' - '.$ex->getTraceAsString());
         }
-
     }
 
     private function migrateShippingCountries() {
